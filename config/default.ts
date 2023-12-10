c27cd8c624fd77758b01cdc1ae69bc80585679a7
maabChaoui <maab.chaoui@ensia.edu.dz>
2023-12-10 15:46:53 +0100

    initial setup

---
+++ config/default.ts
@@ -0,0 +1,7 @@
+export default {
+    origin: 'http://localhost:3000',
+    accessTokenExpiresIn: 60*24*30, // 30 days
+    refreshTokenExpiresIn: 60*24, // 1 day
+    redisCacheExpiresIn: 60,
+  };
+  
---
+++ src/utils/appError.ts
@@ -0,0 +1,18 @@
+export default class AppError extends Error {
+  status: string;
+  error: {
+    message: string;
+  };
+  isOperational: boolean;
+  constructor(public statusCode: number = 500, public message: string) {
+    super(message);
+    this.error = {
+      message,
+    };
+    // this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+    this.status = `${statusCode}`;
+    this.isOperational = true;
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
